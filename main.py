7f1354a685f41c00a4dcbb8123371433081bf068
abduhamid1 <abduhamid3222@gamil.com>
2025-04-30 18:24:52 +0300

    ifix

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from aiogram.fsm.storage.memory import MemoryStorage
-from handlers import menu
 from config import BOT_TOKEN
-from aiogram.client.default import DefaultBotProperties
 from handlers import menu, message_handler
 
 import asyncio
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+    bot = Bot(token=BOT_TOKEN, parse_mode="HTML")  # Bu yerda to'g'ri
     dp = Dispatcher(storage=MemoryStorage())
 
     dp.include_router(menu.router)
@@ -19,7 +17,6 @@ async def main():
         BotCommand(command="start", description="Botni ishga tushirish"),
     ])
     
-
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
